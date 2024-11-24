53e5617533d39e605b0d4f96c3904105569d43ec
nimerrenno <156730973+nimerrenno@users.noreply.github.com>
2024-11-24 16:23:51 -0500

    Add files via upload

---
+++ SRC/sql sources/DataDescription.sql
@@ -0,0 +1,49 @@
+use songselector;
+
+
+describe album;
+
+
+describe artist;
+
+
+describe awards;
+
+
+describe collaboration;
+
+
+describe company;
+
+
+describe genre;
+
+
+describe label;
+
+
+describe liveevents;
+
+
+describe member;
+
+
+describe membership;
+
+
+describe multigenretype;
+
+
+describe performancetype;
+
+
+describe producer;
+
+
+describe production;
+
+
+describe region;
+
+
+describe song;
